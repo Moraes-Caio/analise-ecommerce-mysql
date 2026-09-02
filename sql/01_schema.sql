77d672c446de333b776306ba210b3bbe43538025
Moraes-Caio <dmcaio.24@gmail.com>
2026-09-02 11:59:31 -0300

    test: visualizar e verificar tabelas
    Visualizar as 6 tabelas com os dados carregados para certificar que estão corretas.

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -5,3 +5,91 @@ CREATE DATABASE IF NOT EXISTS olist
     
 USE olist;
 
+-- Deletando Tabelas caso seja necessario rodar o query novamente
+DROP TABLE IF EXISTS order_reviews;
+DROP TABLE IF EXISTS order_items;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS product_category_name_translation;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS customers;
+
+
+-- Criando tabela customers
+CREATE TABLE customers (
+	customer_id VARCHAR(32),
+    customer_unique_id VARCHAR(32) NOT NULL,
+    customer_zip_code_prefix CHAR(5),
+    customer_city VARCHAR(50),
+    customer_state CHAR(2),
+    CONSTRAINT pk_customer PRIMARY KEY (customer_id)
+    );
+    
+    
+-- Criando tabela products
+CREATE TABLE products (
+	product_id VARCHAR(32),
+	product_category_name VARCHAR(50),
+	product_name_lenght SMALLINT UNSIGNED,
+	product_description_lenght SMALLINT UNSIGNED,
+	product_photos_qty SMALLINT UNSIGNED,
+	product_weight_g MEDIUMINT UNSIGNED,
+	product_length_cm SMALLINT UNSIGNED,
+	product_height_cm SMALLINT UNSIGNED,
+	product_width_cm SMALLINT UNSIGNED,
+    CONSTRAINT pk_products PRIMARY KEY (product_id)
+	);
+        
+        
+-- Criando tabela product_category_name_translation
+CREATE TABLE product_category_name_translation (
+	product_category_name VARCHAR(50),
+	product_category_name_english VARCHAR(50),
+    CONSTRAINT pk_product_category_name_translation PRIMARY KEY (product_category_name)
+	);
+        
+        
+-- Criando tabela orders
+CREATE TABLE orders (
+	order_id VARCHAR(32),
+    customer_id VARCHAR(32) NOT NULL,
+    order_status VARCHAR(20),
+    order_purchase_timestamp DATETIME,
+    order_approved_at DATETIME, 
+    order_delivered_carrier_date DATETIME,
+    order_delivered_customer_date DATETIME,
+    order_estimated_delivery_date DATETIME,
+    CONSTRAINT pk_orders PRIMARY KEY (order_id),
+    CONSTRAINT fk_orders_product_id FOREIGN KEY (customer_id)
+    REFERENCES customers(customer_id)
+    );
+
+
+-- Criando tabela order_items
+CREATE TABLE order_items (
+	order_id VARCHAR(32) NOT NULL,
+    order_item_id TINYINT UNSIGNED,
+    product_id VARCHAR(32) NOT NULL,
+    seller_id VARCHAR(32),
+    shipping_limit_date DATETIME,
+    price DECIMAL(10,2),
+    freight_value DECIMAL(10,2),
+    CONSTRAINT pk_order_item_id PRIMARY KEY (order_id, order_item_id),
+    CONSTRAINT fk_order_items_order_id FOREIGN KEY (order_id)
+    REFERENCES orders(order_id),
+    CONSTRAINT fk_order_items_product_id FOREIGN KEY (product_id)
+    REFERENCES products(product_id)
+    );
+    
+    
+-- Criando tabela order_reviews
+CREATE TABLE order_reviews (
+	review_id VARCHAR(32),
+	order_id VARCHAR(32) NOT NULL,
+	review_score TINYINT UNSIGNED NOT NULL,
+	review_comment_title TEXT,
+	review_comment_message MEDIUMTEXT,
+	review_creation_date DATETIME,
+	review_answer_timestamp DATETIME,
+	CONSTRAINT fk_order_reviews_order_id FOREIGN KEY (order_id)
+	REFERENCES orders(order_id)
+	);
--- sql/02_load.sql
+++ sql/02_load.sql
@@ -3,64 +3,102 @@ SHOW GLOBAL VARIABLES LIKE 'local_infile';
 SET GLOBAL local_infile = 1;
 SHOW GLOBAL VARIABLES LIKE 'local_infile';
 
+
 -- Upload de olist_customers_dataset.csv
 LOAD DATA LOCAL INFILE 'C:/Users/cdm24/codigo/olist_sql/olist/olist_customers_dataset.csv'
 INTO TABLE customers
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\n' -- Unix (LF)
 IGNORE 1 ROWS
 (customer_id, customer_unique_id, customer_zip_code_prefix, 
 customer_city, customer_state);
+
 
 -- Upload de olist_products_dataset.csv
 LOAD DATA LOCAL INFILE 'C:/Users/cdm24/codigo/olist_sql/olist/olist_products_dataset.csv'
 INTO TABLE products
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\n' -- Unix (LF)
 IGNORE 1 ROWS
-(product_id, product_category_name, product_name_lenght, 
-product_description_lenght, product_photos_qty, product_weight_g, 
-product_length_cm, product_height_cm, product_width_cm);
+(product_id, product_category_name, @product_name_lenght, 
+@product_description_lenght, @product_photos_qty, @product_weight_g, 
+@product_length_cm, @product_height_cm, @product_width_cm)
+SET
+  product_name_lenght = NULLIF(@product_name_lenght, ''),
+  product_description_lenght = NULLIF(@product_description_lenght, ''),
+  product_photos_qty = NULLIF(@product_photos_qty, ''),
+  product_weight_g = NULLIF(@product_weight_g, ''),
+  product_length_cm = NULLIF(@product_length_cm, ''),
+  product_height_cm = NULLIF(@product_height_cm, ''),
+  product_width_cm = NULLIF(@product_width_cm, '');
+  
 
 -- Upload de olist_product_category_name_translation.csv
 LOAD DATA LOCAL INFILE 'C:/Users/cdm24/codigo/olist_sql/olist/product_category_name_translation.csv'
 INTO TABLE product_category_name_translation
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\r\n' -- Windows (CRLF)
 IGNORE 1 ROWS
 (product_category_name, product_category_name_english);
+
 
 -- Upload de olist_orders_dataset.csv
 LOAD DATA LOCAL INFILE 'C:/Users/cdm24/codigo/olist_sql/olist/olist_orders_dataset.csv'
 INTO TABLE orders
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\n' -- Unix (LF)
 IGNORE 1 ROWS
 (order_id, customer_id, order_status, order_purchase_timestamp, 
-order_approved_at, order_delivered_carrier_date, 
-order_delivered_customer_date,order_estimated_delivery_date);
+@order_approved_at, @order_delivered_carrier_date, 
+@order_delivered_customer_date, order_estimated_delivery_date)
+SET
+	order_approved_at = NULLIF(@order_approved_at, ''),
+    order_delivered_carrier_date = NULLIF(@order_delivered_carrier_date, ''),
+    order_delivered_customer_date = NULLIF(@order_delivered_customer_date, '');
+
 
 -- Upload de olist_order_items_dataset.csv
 LOAD DATA LOCAL INFILE 'C:/Users/cdm24/codigo/olist_sql/olist/olist_order_items_dataset.csv'
 INTO TABLE order_items
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\n' -- Unix (LF)
 IGNORE 1 ROWS
 (order_id, order_item_id, product_id, seller_id, 
 shipping_limit_date, price, freight_value);
+
 
 -- Upload de olist_order_reviews_dataset.csv
 LOAD DATA LOCAL INFILE 'C:/Users/cdm24/codigo/olist_sql/olist/olist_order_reviews_dataset.csv'
 INTO TABLE order_reviews
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\r\n' -- Windows (CRLF)
 IGNORE 1 ROWS
-(review_id, order_id, review_score, review_comment_title, 
-review_comment_message, review_creation_date, 
-review_answer_timestamp);
+(review_id, order_id, review_score, @review_comment_title, 
+@review_comment_message, review_creation_date, 
+review_answer_timestamp)
+SET
+	review_comment_title = NULLIF(@review_comment_title, ''),
+    review_comment_message = NULLIF(@review_comment_message, '');
+
+
+-- Certificando quantidade de linhas
+SELECT COUNT(*) FROM customers; -- CONFIRMADO
+SELECT COUNT(*) FROM products; -- CONFIRMADO
+SELECT COUNT(*) FROM product_category_name_translation; -- CONFIRMADO
+SELECT COUNT(*) FROM orders; -- CONFIRMADO
+SELECT COUNT(*) FROM order_items; -- CONFIRMADO
+SELECT COUNT(*) FROM order_reviews; -- CONFIRMADO
+
+-- Visualizando as tabelas
+SELECT * FROM customers;
+SELECT * FROM products;
+SELECT * FROM product_category_name_translation;
+SELECT * FROM orders;
+SELECT * FROM order_items;
+SELECT * FROM order_reviews;
